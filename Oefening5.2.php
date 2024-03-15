7fc893421a7d5117a933db08f17f71f22a698bd3
CryoCode <owenramaekers2006@gmail.com>
2024-03-15 12:18:07 +0100

    modified:   Oefening5.2.php

--- Oefening5.2.php
+++ Oefening5.2.php
@@ -6,40 +6,40 @@
 </head>
 <body>
     <form>
-            <table>
-                <tr>
-                    <td colspan="2">
-                        <h1>Inloggen</h1>
-                        <p>
-                            Welkom op onze website. <br>
-                            Na correct in te loggen, krijgt u toegang tot onze schatkist. Deze is dan voor u, om te gebruiken!
-                        </p>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        Username
-                    </td>
-                    <td>
-                        <input type="text">
-                    </td>
-                </tr>
+        <table>
+            <tr>
+                <td colspan="2">
+                    <h1>Inloggen</h1>
+                    <p>
+                        Welkom op onze website. <br>
+                        Na correct in te loggen, krijgt u toegang tot onze schatkist. Deze is dan voor u, om te gebruiken!
+                    </p>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Username
+                </td>
+                <td>
+                    <input type="text">
+                </td>
+            </tr>
 
-                <tr>
-                    <td>
-                        Password
-                    </td>
-                    <td>
-                        <input type="password">
-                    </td>
-                </tr>
+            <tr>
+                <td>
+                    Password
+                </td>
+                <td>
+                    <input type="password">
+                </td>
+            </tr>
 
-                <tr>
-                    <td colspan="">
-                        <input type="submit" value="Verzenden">
-                    </td>
-                </tr>
-            </table>
+            <tr>
+                <td colspan="2">
+                    <input type="submit" value="Verzenden">
+                </td>
+            </tr>
+        </table>
     </form>
 </body>
 </html>
