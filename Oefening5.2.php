84e2cff041091d63b1d07b4b86a095e0805772bd
CryoCode <owenramaekers2006@gmail.com>
2024-03-15 12:16:28 +0100

    modified:   Oefening5.2.php

---
+++ Oefening5.2.php
@@ -0,0 +1,45 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form>
+            <table>
+                <tr>
+                    <td colspan="2">
+                        <h1>Inloggen</h1>
+                        <p>
+                            Welkom op onze website. <br>
+                            Na correct in te loggen, krijgt u toegang tot onze schatkist. Deze is dan voor u, om te gebruiken!
+                        </p>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Username
+                    </td>
+                    <td>
+                        <input type="text">
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        Password
+                    </td>
+                    <td>
+                        <input type="password">
+                    </td>
+                </tr>
+
+                <tr>
+                    <td colspan="">
+                        <input type="submit" value="Verzenden">
+                    </td>
+                </tr>
+            </table>
+    </form>
+</body>
+</html>
